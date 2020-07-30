5a324576630c4114114612bb842fce3bb549b614
Rovshan Musayev <arlington1985@gmail.com>
2020-07-31 00:02:21 +0200

    Fix double prompt issue

--- examples/hierarchical.py
+++ examples/hierarchical.py
@@ -14,7 +14,7 @@ def ask_direction():
         'message': 'Which direction would you like to go?',
         'choices': ['Forward', 'Right', 'Left', 'Back']
     }
-    answers = prompt.prompt(directions_prompt)
+    answers = prompt(directions_prompt)
     return answers['direction']
 
 # TODO better to use while loop than recursion!
@@ -67,7 +67,7 @@ def encounter2a():
 
 
 def encounter2b():
-    prompt.prompt({
+    prompt({
         'type': 'list',
         'name': 'weapon',
         'message': 'Pick one',
