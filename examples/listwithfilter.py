28c449a2a8f5d2e49442698611af14d5def2b742
Tim Harris <tim.harris@cfacorp.com>
2020-10-26 16:56:02 -0400

    Adding list prompt that filters when you type

---
+++ examples/listwithfilter.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+list prompt example
+"""
+from __future__ import print_function, unicode_literals
+
+from pprint import pprint
+
+from PyInquirer import prompt, Separator
+
+from examples import custom_style_2
+
+
+
+def get_delivery_options(answers):
+    options = ['bike', 'car', 'truck']
+    if answers['size'] == 'jumbo':
+        options.append('helicopter')
+    return options
+
+
+questions = [
+    {
+        'type': 'list',
+        'name': 'theme',
+        'message': 'What do you want to do?',
+        'choices': [
+            'Order a pizza',
+            'Make a reservation',
+            Separator(),
+            'Ask for opening hours',
+            {
+                'name': 'Contact support',
+                'disabled': 'Unavailable at this time'
+            },
+            'Talk to the receptionist'
+        ]
+    },
+    {
+        'type': 'list',
+        'name': 'size',
+        'message': 'What size do you need?',
+        'choices': ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+        'filter': lambda val: val.lower()
+    },
+    {
+        'type': 'list',
+        'name': 'delivery',
+        'message': 'Which vehicle you want to use for delivery?',
+        'choices': get_delivery_options,
+    },
+]
+
+answers = prompt.prompt(questions, style=custom_style_2)
+pprint(answers)
---
+++ tests/test_example_listwithfilter.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import textwrap
+
+from .helpers import keys
+from .helpers import create_example_fixture
+
+
+example_app = create_example_fixture('examples/listwithfilter.py')
+
+
+def test_list(example_app):
+    example_app.expect(textwrap.dedent("""\
+        ? What do you want to do?  (Use arrow keys)
+         ❯ Order a pizza
+           Make a reservation
+          ---------------
+           Ask for opening hours
+           - Contact support (Unavailable at this time)
+           Talk to the receptionist"""))
+    example_app.write(keys.ENTER)
+    example_app.expect(textwrap.dedent("""\
+        ? What do you want to do?  Order a pizza
+        ? What size do you need?  (Use arrow keys)
+         ❯ Jumbo
+           Large
+           Standard
+           Medium
+           Small
+           Micro"""))
+    example_app.write(keys.ENTER)
+    example_app.expect(textwrap.dedent("""\
+        ? What size do you need?  Jumbo
+        ? Which vehicle you want to use for delivery?  (Use arrow keys)
+         ❯ bike
+           car
+           truck
+           helicopter"""))
+    example_app.write(keys.ENTER)
+    example_app.expect(textwrap.dedent("""\
+        ? Which vehicle you want to use for delivery?  bike
+        {'delivery': 'bike', 'size': 'jumbo', 'theme': 'Order a pizza'}
+        """))
